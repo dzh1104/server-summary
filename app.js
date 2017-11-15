919c2be50053b56bc0fb195858c8a2641e1bf6aa
丁昭华 <1207470844@qq.com>
2017-11-15 22:43:43 +0800

    解析对应请求的数据

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
-const server = require('koa-static');
+const path = require('path');
+const serve = require('koa-static');
 
 const app = new Koa();
 
 const controller = require('./controller');
+
+app.use(serve(path.resolve(__dirname, '../client-summary/dist')));
 
 app.use(controller());
 
--- controllers/test.js
+++ controllers/test.js
@@ -1,5 +1,7 @@
 module.exports = {
   'get /test': async (ctx, next) => {
+    let query = JSON.parse(ctx.query.data);
+    console.log('query.name', query.name);
     ctx.body = 'hello world !'
   }
 };
