8fd34171121d03921857cbf68b2300cfd4fe7d8c
dingzhaohua <1207470844@qq.com>
2017-11-16 16:47:07 +0800

    暂不加密【暂定】query取得数据改变
    暂不加密【暂定】query取得数据改变

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,6 @@
 module.exports = {
   'get /test': async (ctx, next) => {
-    let query = ctx.query.data;
+    let query = ctx.query;
     console.log('query.name', query);
     ctx.body = {
       query
