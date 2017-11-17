bb76bdf6da13f80ad6795bdb26090a055884d3d1
丁昭华 <1207470844@qq.com>
2017-11-17 22:49:19 +0800

    线上测试环境部署

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,9 @@
 module.exports = {
     'post /login': async (ctx, next) => {
+        let body = ctx.request.body;
         ctx.body = {
-            message: 'success'
+            message: 'success',
+            body
         }
     }
 };
