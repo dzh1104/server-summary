53a9da467ac787b6ba3d3675e78eae5563c8f8fb
丁昭华 <1207470844@qq.com>
2017-11-18 23:47:51 +0800

    数据库安装和使用的补充

---
+++ src/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const DB_URL = 'mongodb://localhost:27017/mongoosesample';
+
+/**
+ * 连接
+ */
+mongoose.connect(DB_URL);
+
+
+/**
+ * 连接成功
+ */
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connection open to ' + DB_URL);
+});
+
+/**
+ * 连接异常
+ */
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error: ' + err);
+});
+
+/**
+ * 连接断开
+ */
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose connection disconnected');
+});
