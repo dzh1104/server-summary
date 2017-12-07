7884dc385cc97d76defa9bc51d287e0bdae93898
dzh1104 <1207470844@qq.com>
2017-12-07 22:39:00 +0800

    Revert "更改返回role"
    This reverts commit f8ed0790a955a75f601d991bfe113fb939862231.

--- app/summary/controllers/auth.js
+++ app/summary/controllers/auth.js
@@ -95,7 +95,7 @@ module.exports = {
 
     'get /auth/info': async(ctx, next) => {
         ctx.rest({
-            roles: ['admin'],
+            roles: ['admin2'],
             introduction: '我是超级管理员',
             avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
             username: 'Super Admin'
