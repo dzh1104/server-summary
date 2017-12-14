d8c87b83b4ffc80a99226cc6b767d73f496c6b0a
dzh1104 <1207470844@qq.com>
2017-12-14 17:09:54 +0800

    middle的开发
    middle的开发

--- middlewares/getReqData/index.js
+++ middlewares/getReqData/index.js
@@ -21,3 +21,9 @@ const getReqData = () => {
 };
 
 module.exports = getReqData;
+
+// middles的开发 返回一个async函数 app.use(configMiddle())
+
+// 可以让异步逻辑用同步写法实现
+// 最底层的await返回需要是Promise对象
+// 可以通过多层 async function 的同步写法代替传统的callback嵌套
